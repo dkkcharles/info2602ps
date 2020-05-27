9672b3676b1c2ad15a7a0480dc92cc6d6978aad3
dkkcharles <dkkcharles@gmail.com>
2020-05-27 23:48:16 +0000

    models

--- models.py
+++ models.py
@@ -3,16 +3,54 @@ db = SQLAlchemy()
 import datetime
 
 
-class Logs(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    studentId =  db.Column(db.Integer, nullable=False)
-    stream = db.Column(db.Integer, nullable=False)
-    created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def toDict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "password": self.password
+            }
+    
+    def set_password(self,password):
+        self.password = generate_password_hash(password,method='sha256')
+
+    def check_password(self,password):
+        return check_password_hash(self.password,password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class UserReact(db.Model):
+    id = db.Column('id', db.Integer, db.ForeignKey('user.id'))
+    postid = db.Column('postid', db.Integer, db.ForeignKey('post.postid'))
+    react = db.Column(db.String('like'|'dislike')
+
+
+class Post(db.Model):
+    postid = db.Column(db.Integer, primary_key =True)
+    id = db.Column(db.Integer, db.ForeignKey('user.id'),)
+    text = db.column(db.String(280), nullable=False)
+    reacts = db.relationship('UserReact')
+
+    def getTotalLikes():
+        return{
+            total
+        }
+    
+    def getTotalDislikes():
+        return{
+            total
+        }
 
     def toDict(self):
         return{
-            'id': self.id,
-            'studentId': self.studentId,
-            'stream': self.stream,
-            'created': self.created.strftime("%m/%d/%Y, %H:%M:%S")
+            "User" : self.user.username,
+            "Likes" : self.getTotalLikes,
+            "Dislikes" : self.getTotalDislikes
         }
